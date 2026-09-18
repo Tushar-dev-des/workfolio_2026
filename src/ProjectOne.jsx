af4084af012e391405c2105c65f92d8b40098dfc
Tushar Mahajan <tushardm.2001@gmail.com>
2026-09-18 13:30:44 +0530

    changed project one copy

--- src/ProjectOne.jsx
+++ src/ProjectOne.jsx
@@ -188,7 +188,7 @@ const ProjectOne = () => {
                         <div className="my_role">
                             <p style={{ fontWeight: "600", color: "#344054" }}>My role</p>
                             <p>I owned end-to-end experience design: right from the ideation through execution, from email notifications to edge cases; <br /> along with shaping and defining product strategy. </p>
-                            <p>Qualitative user & product research. Beyond our existing users, I partnered with sales to speak with competitors' users too, who surfaced pain points and mental models we wouldn't have found otherwise.</p>
+                            <p> Led qualitative user & product research to uncover adoption barriers, workflow inefficiencies and critical insights that shaped our approach in designing the new experience.</p>
                             <p>Worked closely with cross-functional teams and stakeholders, both directly and indirectly tied to the project, to keep decisions aligned and execution on track.</p>
                         </div>
                     </div>
